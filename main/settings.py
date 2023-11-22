9e6279376d63169ef185452d5cfc89b4213739a6
rzpy1997 <kingradomir@gmail.com>
2023-11-22 13:43:36 +0600

    'lesson'

--- main/settings.py
+++ main/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'win_1_21_db',
+        "PORT": 5432,
+        "HOST": "localhost",
+        "USER": "your_username",
+        "PASSWORD": 'your_password',
     }
 }
 
